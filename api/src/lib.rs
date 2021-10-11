98ecdef9e1d895d2e739899afdafef168f4aa2e5
DanieleDiBenedetto <daniele@horizenlabs.io>
2021-10-11 11:17:49 +0200

    Now possible to use api crate as external dependency in another project

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,36 +1,3 @@
-#![deny(
-    unused_import_braces,
-    unused_qualifications,
-    trivial_casts,
-    trivial_numeric_casts
-)]
-#![deny(
-    unused_qualifications,
-    variant_size_differences,
-    stable_features,
-    unreachable_pub
-)]
-#![deny(
-    non_shorthand_field_patterns,
-    unused_attributes,
-    unused_imports,
-    unused_extern_crates
-)]
-#![deny(
-    renamed_and_removed_lints,
-    stable_features,
-    unused_allocation,
-    unused_comparisons,
-    bare_trait_objects
-)]
-#![deny(
-    const_err,
-    unused_must_use,
-    unused_mut,
-    unused_unsafe,
-    private_in_public,
-)]
-
 use algebra::{SemanticallyValid, serialize::*};
 use std::any::type_name;
 
